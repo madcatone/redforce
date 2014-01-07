3973fe77b26b8ffc23e4d7baf88e9a9197f5944c
rubyinhell <rubyinhell@gmail.com>
2014-01-07 14:30:36 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redforce::Application.config.secret_key_base = 'b85eb3ac6a5535cda6bb847383341f42bbbb69f37c2aa0baf7c816a3a4caf7d5248a71f1affce1b9b65f5f2bb9cfad1202106894344b340975ee16de1ffc9df7'
