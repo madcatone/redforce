b7ae63db38ffdb2cb592a1ec94857a4228fb1942
rubyinhell <rubyinhell@gmail.com>
2015-02-26 19:25:27 +0800

    modify problems 2

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,6 @@
 class OtherProblem < ActiveRecord::Base
-	establish_connection "problem_database_development"
-	self.table_name = "2015-01-20_problem_mail_cons"
+	#establish_connection "problem_database_development"
+	#self.table_name = "2015-01-20_problem_mail_cons"
 end
 
 class Problem < OtherProblem
